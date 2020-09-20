1f46595b99b11916ace873787ee7bc54f436d80e
inherent <himanshu648594@gmail.com>
2020-09-20 14:42:39 +0530

    add cart page and auth pages

--- server.js
+++ server.js
@@ -6,14 +6,27 @@ const expressLayout  = require('express-ejs-layouts');
 
 app.use(express.static('public'));
 
-app.get('/',(req, res)=>{
-    res.render('home')
-})
+
 //set template engine
 app.use(expressLayout);
 
 app.set('views',path.join(__dirname,'/resources/views/'));
 app.set('view engine', 'ejs');
+
+app.get('/',(req, res)=>{
+    res.render('home')
+})
+app.get('/cart',(req, res)=> {
+  res.render('customers/cart')
+});
+app.get('/login',(req, res)=> {
+  res.render('auth/login')
+})
+
+app.get('/register',(req, res)=> {
+  res.render('auth/register')
+})
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
