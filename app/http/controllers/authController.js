672ea1058eeae87d11a3cfe17f12d3ed90efbb4c
inherent <himanshu648594@gmail.com>
2020-09-28 20:16:18 +0530

    authentication added and logout

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -1,12 +1,88 @@
-function authController(){
-    return {
-        login (req, res) {
-            res.render('auth/login');
-        },
-        register (req, res) {
-            res.render('auth/register');
+const User = require("../../models/user");
+const bcrypt = require('bcrypt');
+const passport = require("passport");
+function authController() {
+  return {
+    logout(req, res){
+      req.logout()
+      return res.redirect('/login')
+    },
+    login(req, res) {
+      res.render("auth/login");
+    },
+    postLogin(req, res, next){
+      passport.authenticate('local',(err, user, info)=>{
+        if(err){
+      
+
+          req.flash('error',info.message)
+          return next(err)
         }
-    }
+        if(!user){
+          req.flash('error',info.message)
+          return res.redirect('/login')
+        }
+
+        req.logIn(user, (err)=> {
+          if(err){
+            console.log("post login call",err);
+            req.flash('error',info.message)
+            return next(err)
+          }
+          return res.redirect('/');
+        })
+      })(req, res, next);
+    },
+    register(req, res) {
+      res.render("auth/register");
+    },
+    async postRegister(req, res) {
+      console.log("request called");
+      const { name, email, password } = req.body;
+      //validate request
+
+      if (!name || !email || !password) {
+        req.flash("error", "All fields are required");
+        req.flash("name", name);
+        req.flash("email", email);
+
+       return res.redirect("/register");
+      }
+      //check email
+      User.exists({ email: email }, (err, result) => {
+        if (result) {
+          req.flash("error", "Email Already taken");
+          req.flash("name", name);
+          req.flash("email", email);
+
+         return res.redirect("/register");
+        }
+      });
+      //Hash Password
+      const hashedPassword = await bcrypt.hash(password,10);
+
+      //Create User
+      const user = new User({
+          name,
+          email,
+          password:hashedPassword
+      })
+      user.save().then((user)=>{
+        //Login
+
+        return res.redirect('/');
+      }).catch(err => {
+        req.flash("error", "Something went wrong");
+        
+
+       return res.redirect("/register");
+
+      });
+      console.log(req.body);
+    },
+    
+  };
+
 }
 
 module.exports = authController;
--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("express-flash");
 const expressLayout = require("express-ejs-layouts");
+const passport = require('passport');
 const MongoDbStore = require("connect-mongo")(session);
 app.use(express.static("public"));
 
 
 //express json
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 
 //Database Connection
 const url = "mongodb://localhost/pizza";
@@ -48,11 +50,18 @@ app.use(
     cookie: { maxAge: 1000 * 60 * 60 * 24 },
   })
 );
+
+//Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session());
 //set template engine
 app.use(expressLayout);
 //global middleware
 app.use((req, res, next)=>{
   res.locals.session = req.session;
+  res.locals.user = req.user;
   next();
 })
 app.set("views", path.join(__dirname, "/resources/views/"));
