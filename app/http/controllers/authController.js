595cd3393de37f9eafd087f4d7bddb80c34ea4c1
inherent <himanshu648594@gmail.com>
2020-10-11 07:45:24 +0530

    added orders features

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -11,6 +11,13 @@ function authController() {
       res.render("auth/login");
     },
     postLogin(req, res, next){
+      const { email, password } = req.body;
+      if (!email || !password) {
+        req.flash("error", "* All fields are required");
+        
+
+       return res.redirect("/login");
+      }
       passport.authenticate('local',(err, user, info)=>{
         if(err){
       
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import Noty from 'noty';
+
+import {initAdmin} from './admin';
 let addToCart = document.querySelectorAll('.add-to-cart');
 let cartCounter = document.querySelector('#cartCounter');
 function updateCart(pizza){
@@ -29,4 +31,13 @@ addToCart.forEach((btn)=> {
 
         console.log(pizza)
     })
-})
+});
+
+//remove alert message
+const alertMsg = document.querySelector("#success-alert");
+if(alertMsg) {
+    setTimeout(() => {
+        alertMsg.remove()
+    }, 2000);
+}
+initAdmin();
